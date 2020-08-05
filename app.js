0e487af146bf2464ff5bb10f16eb4361af394b25
FarwaAbbas123 <60059290+FarwaAbbas123@users.noreply.github.com>
2020-08-05 16:16:36 +0500

    Quiz_App

---
+++ app.js
@@ -0,0 +1,170 @@
+function checkResult(){
+    var score = 0;
+
+    var ans1 = document.getElementById('q1');
+    var ans2 = document.getElementById('q2');
+    var ans3 = document.getElementById('q3');
+    var ans4 = document.getElementById('q4');
+
+    if(ans1.checked ==  true){
+        score++
+        alert("Question 1 is correct")
+
+    }
+
+    else{
+        alert("Question 1 is wrong")
+    }
+
+
+    var ans5 = document.getElementById('q5');
+    var ans6 = document.getElementById('q6');
+    var ans7 = document.getElementById('q7');
+    var ans8 = document.getElementById('q8');
+
+    if(ans7.checked ==  true){
+        score++
+        alert("Question 2 is correct")
+
+    }
+
+    else{
+        alert("Question 2 is wrong")
+    }
+
+    var ans9 = document.getElementById('q9');
+    var ans10 = document.getElementById('q10');
+    var ans11 = document.getElementById('q11');
+    var ans12 = document.getElementById('q12');
+
+    if(ans10.checked ==  true){
+        score++
+        alert("Question 3 is correct")
+
+    }
+
+    else{
+        alert("Question 3 is wrong")
+    }
+
+
+
+    
+    var ans13 = document.getElementById('q13');
+    var ans14 = document.getElementById('q14');
+    var ans15 = document.getElementById('q15');
+    var ans16 = document.getElementById('q16');
+
+    if(ans16.checked ==  true){
+        score++
+        alert("Question 4 is correct")
+
+    }
+
+    else{
+        alert("Question 4 is wrong")
+    }
+
+
+
+    var ans17 = document.getElementById('q17');
+    var ans18 = document.getElementById('q18');
+    var ans19 = document.getElementById('q19');
+    var ans20 = document.getElementById('q20');
+
+    if(ans18.checked ==  true){
+        score++
+        alert("Question 5 is correct")
+
+    }
+
+    else{
+        alert("Question 5 is wrong")
+    }
+
+
+    
+    var ans21 = document.getElementById('q21');
+    var ans22 = document.getElementById('q22');
+    var ans23 = document.getElementById('q23');
+    var ans24 = document.getElementById('q24');
+
+    if(ans24.checked ==  true){
+        score++
+        alert("Question 6 is correct")
+
+    }
+
+    else{
+        alert("Question 6 is wrong")
+    }
+
+    var ans25 = document.getElementById('q25');
+    var ans26 = document.getElementById('q26');
+    var ans27 = document.getElementById('q27');
+    var ans28 = document.getElementById('q28');
+
+    if(ans25.checked ==  true){
+        score++
+        alert("Question 7 is correct")
+
+    }
+
+    else{
+        alert("Question 7 is wrong")
+    }
+
+
+    var ans29 = document.getElementById('q29');
+    var ans30 = document.getElementById('q30');
+    var ans31 = document.getElementById('q31');
+    var ans32 = document.getElementById('q32');
+
+    if(ans31.checked ==  true){
+        score++
+        alert("Question 8 is correct")
+
+    }
+
+    else{
+        alert("Question 8 is wrong")
+    }
+
+
+
+    var ans33 = document.getElementById('q33');
+    var ans34 = document.getElementById('q34');
+    var ans35 = document.getElementById('q35');
+    var ans36 = document.getElementById('q36');
+
+    if(ans36.checked ==  true){
+        score++
+        alert("Question 9 is correct")
+
+    }
+
+    else{
+        alert("Question 9 is wrong")
+    }
+
+
+
+    var ans37 = document.getElementById('q37');
+    var ans38 = document.getElementById('q38');
+    var ans39 = document.getElementById('q39');
+    var ans40 = document.getElementById('q40');
+
+    if(ans38.checked ==  true){
+        score++
+        alert("Question 10 is correct")
+
+    }
+
+    else{
+        alert("Question 10 is wrong")
+    }
+
+   
+    alert("Your Score Is " + score)
+
+} 
